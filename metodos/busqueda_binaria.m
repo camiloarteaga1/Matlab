52329ab3d2d753e38398a723623ae66fbcde0f53
Daniel <daniel.canor1@udea.edu.co>
2021-12-03 19:04:17 -0500

    0.0
    actulizacion busqueda binaria

--- metodos/busqueda_binaria.m
+++ metodos/busqueda_binaria.m
@@ -1,23 +1,32 @@
 clear all
-a1=input('x1 inicial= ');
-b1=input('x2 final= ');
+a1=input('lim inf= ');
+b1=input('lim sup= ');
 syms x y(x) root root2
 y(x)=input('y fun= ');
-
+state=0;
 if y(a1)>0
     z1=a1;
     a1=b1;
     b1=z1;
+    state=1;
 end
 
 root=(b1+a1)/2;
-fprintf('root = %s \n ',root)
-   fprintf('y(root) = %s \n ',eval(y(root)))
+disp('| k | a | b | f(a) | f(b) | root | f(root) | b-a |');
+k=0;
 if (y(a1)<0 && y(b1)>0) || (y(a1)>0 && y(b1)<0)
     
     while abs(y(root))>=0.0001
+        k=k+1;
         root2=root;
+        if state==0
+            inf=eval([k,a1,b1,y(a1),y(b1),root,y(root),b1-a1])
+        else
+            inf=eval([k,b1,a1,y(b1),y(a1),root,y(root),a1-b1])
+        end
+
         if y(root)<0
+            
             a1=root;
 
         else
@@ -25,10 +34,8 @@ if (y(a1)<0 && y(b1)>0) || (y(a1)>0 && y(b1)<0)
 
         end
 
-
+        
         root=(b1+a1)/2;
-        fprintf('root = %s \n ',root)
-        fprintf('y(root) = %s \n ',eval(y(root)))
 
         if abs(root2-root)<0.0001
             break
