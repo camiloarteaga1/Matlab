d3512f96ae0bfeabb21a5a9a175e3f2d574ef468
Daniel <daniel.canor1@udea.edu.co>
2022-02-21 06:35:16 -0500

    0.0

--- circuitos/diagramaBDcasero.m
+++ circuitos/diagramaBDcasero.m
@@ -1,14 +1,22 @@
-function [datosx,datosy] = diagramaBDcasero(x,y,g,l1,l2,n)
+function [datosx,datosy] = diagramaBDcasero(x,y,g,l1,l2,n,type)
 %DIAGRAMABDCASERO Summary of this function goes here
 %   Detailed explanation goes here
-syms s
+syms s kk
 g(s)=g;
 frec=[];
 db=[];
+ang=[];
 k=1;
 frec=logspace(l1,l2,2000);
+if type=='f'
+    f(kk)=eval(20*log10( abs( g( frec(k)*i*2*pi ) ) ) );
+else if type=='a'
+    f(kk)=eval(angle(g( frec(k)*i*2*pi )  ) );
+else
+    disp('diagrama de frecuencias [f] diagrama de fase [a]')
+end
 while k<=length(frec)
-    db(k)=eval(20*log10( abs( g( frec(k)*i*2*pi ) ) ) );
+    db(k)=f(k);
     k=k+1;
 end
 semilogx(frec,db)
